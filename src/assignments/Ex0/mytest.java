f94a00e7d0d1c5e38722567966a80caef54994e4
itamarrot1 <itamarrot32@gmail.com>
2025-11-06 10:09:54 +0200

    adding my code for the functions

---
+++ src/assignments/Ex0/mytest.java
@@ -0,0 +1,9 @@
+package assignments.Ex0;
+
+import static assignments.Ex0.Ex0.*;
+
+public class mytest {
+    public static void main(String[] args){
+        System.out.println(getMthClosestPrimePair(3 , 6));
+    }
+}
